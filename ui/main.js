250a93dfbdb5754a5c08fd20c520900e42a75a8c
Rishabh Singh <singhrishabh670@gmail.com>
2018-03-03 01:17:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,24 @@
 var button= document.getElementById("counter");
-var counter= 0;
+
 button.onclick = function() {
-    //Make a request to the counter end point.
-    //Capture the response and store it in a variable
-    // Render the variable in the correct span
     
-    counter= counter+1;
-    var span= document.getElementById('count')
-    span.innerHTML = counter.toString();
+    //Create a request object
+      var request= new XMLHttpRequest();
+      
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE)   {
+          //Takge some action
+          if(request.status=== 200) {
+              var counter = request.responseText;
+              var span= document.getElementById('count')
+              span.innerHTML = counter.toString();
+          }
+      }
+      //Not done yet
+    };
+    //Make a request
+    request.open('GET', 'http://http://singhrishabh670.imad.hasura-app.io/counter', true);
+    request.send(null);
+  
 };
