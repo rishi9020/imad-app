2238eaa947287820ceb215a4f95646a5b333b83c
Rishabh Singh <singhrishabh670@gmail.com>
2018-03-02 18:37:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var a1= {
+    title: 'Article 1',
+    heading: 'Article one',
+    date: 'Mar 02, 2018' ,
+    
+    content: " <p> This is paragraph on </p>  <p>This is paragraph two</p>  <p>This is paragraph three: The official teaser of Rajinikanth starrer 'Kaala' has been released. The film also stars Nana Patekar, who will reportedly play the antagonist in the film, Huma Qureshi and Eswari Rao among others. Directed by Pa Ranjith and produced by Rajinikanth's son-in-law Dhanush, 'Kaala' is scheduled to release on April 27.</p> "
+}
+
+
+var htmlTemplate: '
+
+<html>
+    
+    <head>
+        
+        <title>
+        Article 1
+    </title>
+     <meta name="viewport" content="width-device-width, initial-scale=0" />
+     
+    <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    
+    <body>
+        
+        <div class= "container">
+            <div>
+            <a href="/" >Home</a>
+        </div>
+        
+        <hr/>
+        
+        <h3>
+            Article One
+        </h3>
+        
+        <hr/>
+        
+        <div>
+            2-03-2018
+        </div>
+        
+        <div>
+            $ content[]
+        </div>
+    </div>
+        
+    </body>
+
+</html>
+
+
+'
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
