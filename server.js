720268490f1a4769ac04757f47403ef7b57cdae7
rishi9020 <singhrishabh670@gmail.com>
2018-03-02 13:37:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res){
+    res.send('Article 1 is requested and is sent to the browser');
+})
+
+app.get('/article-two', function(req, res){
+    res.send('Article 2 is requested and is sent to the browser');
+})
+
+app.get('/article-three', function(req, res){
+    res.send('Article 3 is requested and is sent to the browser');
+})
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
