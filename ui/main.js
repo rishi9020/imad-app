f965ff459ce7e513cf9230427b531866279f7989
Rishabh Singh <singhrishabh670@gmail.com>
2018-03-03 12:34:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function() {
 
 //Submit Name
 
-var nameInput= document.getElementById('name');
-var name= nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -56,6 +55,8 @@ submit.onclick = function() {
       //Not done yet
     };
     //Make a request
+    var nameInput= document.getElementById('name');
+    var name= nameInput.value;
     request.open('GET', 'http://singhrishabh670.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
    
