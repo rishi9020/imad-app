728427edada496862db3804ca5045913a1f71fb6
Rishabh Singh <singhrishabh670@gmail.com>
2018-03-25 02:27:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
+
+var config= {
+    user: 'singhrishabh670',
+    database: 'singhrishabh670',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,6 +82,21 @@ function createTemplate(data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool= new Pool(config);
+app.get('/test-db', function(req,res){
+    pool.query( 'SELECT * from test', function(err,result){
+        if(err){
+            res.status(500).send(err.toStringify());
+            
+        } else{
+            res.semd(JSON.stringify(result));
+        }
+        
+    });
+    
 });
 
 var names= [];
