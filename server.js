e86d25daaaaa2148d93314798b27f3fb1b6e3256
Rishabh Singh <singhrishabh670@gmail.com>
2018-03-25 19:28:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,14 +113,14 @@ app.get('/counter', function(req, res){
     res.send(counter.toString());
 });
 
-app.get('/article/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
     //articleName = article-one
     //var articleName = req.params.articleName;
     pool.query("SELECT * FROM article WHERE title = " + req.params.articleName , function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else{
-           if(result.rows.length===0){
+           if(result.rows.length==0){
                res.status(404).send('Article not found');
            } else{
                var articleData= result.rows[0];
